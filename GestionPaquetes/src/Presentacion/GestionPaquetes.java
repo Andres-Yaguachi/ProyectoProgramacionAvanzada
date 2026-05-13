02116613ade19a54e168e1f3619730168ae7af84
Andres-Yaguachi <andres.yaguachi3@gmail.com>
2026-05-13 02:43:52 -0500

    LogicaRecepcion semiCompleta

--- GestionPaquetes/src/Presentacion/GestionPaquetes.java
+++ GestionPaquetes/src/Presentacion/GestionPaquetes.java
@@ -1,8 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package Presentacion;
+
+import java.util.Date;
 
 /**
  *
@@ -10,11 +8,8 @@ package Presentacion;
  */
 public class GestionPaquetes {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+
     }
-    
+
 }
